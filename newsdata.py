56bc849174f62ddc2438df0d44b648e86c043e4a
call900913 <call900913@icloud.com>
2019-05-03 13:17:52 -0400

    transform sql keywords to uppercase to improve readability

--- newsdata.py
+++ newsdata.py
@@ -12,12 +12,12 @@ cr = db.cursor()
 
 #What are the three most popular articles?
 cr.execute("""
-select articles.title, count
-from articles join
+SELECT articles.title, count
+FROM articles JOIN
   (select path, count(*) from log where status = '200 OK' group by path)
     as something on slug = replace(path, '/article/', '')
-order by count
-desc limit 3
+ORDER BY count
+DESC limit 3
 """)
 result1 = cr.fetchall()
 print('\nA. The 3 most popular articles:')
@@ -36,13 +36,13 @@ AS select author, sum(count)
     from log
     where status = '200 OK'
     group by path) as log1
-on replace(path, '/article/', '')  = slug
-group by author
+   on replace(path, '/article/', '')  = slug
+   group by author
 """)
 cr.execute("""
-select authors.id, authors.name, sum
-from authors join view_01 on authors.id = view_01.author
-order by sum desc
+SELECT authors.id, authors.name, sum
+FROM authors JOIN view_01 ON authors.id = view_01.author
+ORDER BY sum DESC
 """)
 result2 = cr.fetchall()
 print('\n\nB. The authors ranked by popularity:\n')
@@ -54,14 +54,14 @@ while i < len(result2):
 
 #What are the days on which more than 1% of requests lead to errors?
 cr.execute("""
-select date, round(ep, 2)
-from (select a.date, (100.0 * numOfErroneousRequests / totalRequests) ep
+SELECT date, round(ep, 2)
+FROM (select a.date, (100.0 * numOfErroneousRequests / totalRequests) ep
      from (select date(time) as date, count(status) as numOfErroneousRequests
-          from log where status = '404 NOT FOUND' group by date(time)) as a,
+          from log where status = '404 NOT FOUND' group by date(time)) AS a,
           (select date(time) as date, count(*) as totalRequests
-          from log group by date(time)) as b
+          from log group by date(time)) AS b
      where a.date = b.date) as result
-where ep > 1
+WHERE ep > 1
 """)
 result3 = cr.fetchall()
 print('\n\nThe days on which more than 1% of requests lead to 404 error:')
